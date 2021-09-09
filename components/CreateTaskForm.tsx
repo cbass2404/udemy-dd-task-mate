0718acbb25f03191a033b99f7f012b63e262524b
cbass2404 <cory.bass.07@gmail.com>
2021-09-09 14:04:18 -0500

    gave user ability to create new tasks

--- components/CreateTaskForm.tsx
+++ components/CreateTaskForm.tsx
@@ -1,12 +1,34 @@
 import React, { useState } from "react";
+import { useCreateTaskMutation } from "../generated/graphql-frontend";
 
-const CreateTaskForm = () => {
+interface Props {
+  onSuccess: () => void;
+}
+
+const CreateTaskForm: React.FC<Props> = ({ onSuccess }) => {
   const [title, setTitle] = useState("");
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
+  const [createTask, { loading, error }] = useCreateTaskMutation({
+    onCompleted: () => {
+      onSuccess();
+      setTitle("");
+    },
+  });
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!loading) {
+      try {
+        await createTask({ variables: { input: { title } } });
+      } catch (error) {
+        // log error
+      }
+    }
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {error && <p className="alert-error">An error occurrred.</p>}
       <input
         type="text"
         name="title"
