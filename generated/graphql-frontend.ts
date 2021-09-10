59f68bc6be6fe7b0ff265c5c0d9b64c7dfb677f3
cbass2404 <cory.bass.07@gmail.com>
2021-09-10 09:28:09 -0500

    added ability to filter tasks and set the links to active if visited

--- generated/graphql-frontend.ts
+++ generated/graphql-frontend.ts
@@ -111,7 +111,9 @@ export type TaskQuery = (
   )> }
 );
 
-export type TasksQueryVariables = Exact<{ [key: string]: never; }>;
+export type TasksQueryVariables = Exact<{
+  status?: Maybe<TaskStatus>;
+}>;
 
 
 export type TasksQuery = (
@@ -244,8 +246,8 @@ export type TaskQueryHookResult = ReturnType<typeof useTaskQuery>;
 export type TaskLazyQueryHookResult = ReturnType<typeof useTaskLazyQuery>;
 export type TaskQueryResult = Apollo.QueryResult<TaskQuery, TaskQueryVariables>;
 export const TasksDocument = gql`
-    query Tasks {
-  tasks {
+    query Tasks($status: TaskStatus) {
+  tasks(status: $status) {
     id
     title
     status
@@ -265,6 +267,7 @@ export const TasksDocument = gql`
  * @example
  * const { data, loading, error } = useTasksQuery({
  *   variables: {
+ *      status: // value for 'status'
  *   },
  * });
  */
