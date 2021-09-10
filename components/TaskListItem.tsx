a84a7ac61c4a89ef8b285185cdba3bc3853cec90
cbass2404 <cory.bass.07@gmail.com>
2021-09-10 09:36:16 -0500

    added ability to change task from active to completed

--- components/TaskListItem.tsx
+++ components/TaskListItem.tsx
@@ -1,5 +1,10 @@
-import { useEffect } from "react";
-import { Task, useDeleteTaskMutation } from "../generated/graphql-frontend";
+import React, { useEffect } from "react";
+import {
+  Task,
+  TaskStatus,
+  useDeleteTaskMutation,
+  useUpdateTaskMutation,
+} from "../generated/graphql-frontend";
 import Link from "next/link";
 import { Reference } from "@apollo/client";
 
@@ -39,8 +44,30 @@ const TaskListItem: React.FC<Props> = ({ task }) => {
       alert("An error occurred, please try again");
     }
   }, [error]);
+  const [updateTask, { loading: updateTaskLoading, error: updateTaskError }] =
+    useUpdateTaskMutation({ errorPolicy: "all" });
+  const handleStatusChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newStatus = e.target.checked
+      ? TaskStatus.Completed
+      : TaskStatus.Active;
+    updateTask({ variables: { input: { id: task.id, status: newStatus } } });
+  };
+  useEffect(() => {
+    if (updateTaskError) {
+      alert("An error occurred, please try again.");
+    }
+  }, [updateTaskError]);
   return (
     <li key={task.id} className="task-list-item">
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          onChange={handleStatusChange}
+          checked={task.status === TaskStatus.Completed}
+          disabled={updateTaskLoading}
+        />
+        <span className="checkbox-mark">&#10003;</span>
+      </label>
       <Link href="/update/[id" as={`/update/${task.id}`}>
         <a className="task-list-item-title">{task.title}</a>
       </Link>
